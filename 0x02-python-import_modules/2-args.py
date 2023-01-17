b24234f33b5b5ce9b3644d888185f6dae1aaf66c
gamernewone <37631123+gamernewone@users.noreply.github.com>
2023-01-17 16:27:07 +0000

    house of cards 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# print its arguments
+
+if __name__ = "__main__":
+    """Print the addition of all arguments"""
+    import sys
+
+    if len(sys.argv) == 1: print("0 arguments")
+    else:
+        print("{} arguments:".format(len(sys.argv) - 1))
+        for i in range(1, len(sys.argv)):
+            print("{}: {}".format(sys.argv[i], i))
+
