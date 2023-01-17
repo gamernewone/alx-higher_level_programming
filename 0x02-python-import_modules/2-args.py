0d23720294fb00ccfabfc424d6a4a5cfde2f07b3
gamernewone <37631123+gamernewone@users.noreply.github.com>
2023-01-17 16:40:27 +0000

    fixes

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,9 +5,11 @@ if __name__ == "__main__":
     """Print the addition of all arguments"""
     import sys
 
-    if len(sys.argv) == 1: print("0 arguments")
+    if len(sys.argv) == 1:
+        print("0 arguments")
+    elif len(sys.argv) == 2:
+        print("1 argument\n1: {}".format(sys.argv[1]))
     else:
         print("{} arguments:".format(len(sys.argv) - 1))
         for i in range(1, len(sys.argv)):
             print("{}: {}".format(sys.argv[i], i))
-
