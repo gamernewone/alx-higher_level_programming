b86b1bc53a97d674e14024fbfa3120cf6fe71a75
sage <37631123+gamernewone@users.noreply.github.com>
2023-03-13 23:53:08 +0000

    changes2

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,14 +1,10 @@
-#!/usr/env/python3
-
+#!/usr/bin/python3
 def max_integer(my_list=[]):
-    '''Return the max integer in a list'''
-
-    if (len(my_list) == 0):
-        return (None)
-
-    max = my_list[0]
-
-    for i in range(len(my_list)):
-        if my_list[i] > max:
-            max = my_list[i]
-    return max
+    if len(my_list) == 0:
+        return "None"
+    else:
+        max = my_list[0]
+        for i in range(len(my_list)):
+            if my_list[i] > max:
+                max = my_list[i]
+        return max
